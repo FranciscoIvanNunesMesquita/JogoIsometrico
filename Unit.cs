4327f212c2561068d73b9283e3f0f62b433410ce
FranciscoIvanNunesMesquita <ivannunes.m@hotmail.com>
2019-05-15 12:46:25 -0300

    Add files via upload

---
+++ Unit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [HideInInspector]
+    public Stats stats;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        stats = GetComponentInChildren<Stats>();
+    }
+
+
+}
